1568d9cfca5a43cdbddff6361fa642bc322e12a7
mayakeren55 <81112493+mayakeren55@users.noreply.github.com>
2021-03-21 22:43:12 +0200

    Just a first try

--- scripts/move_turtlebot3.py
+++ scripts/move_turtlebot3.py
@@ -15,11 +15,19 @@ def move_line():
 
 
 def move_circle():
-    print('COMPLETE')
+    rospy.init_node('twist_publisher')
+    twist = Twist()
+    twist.angular.z = 0.7
+    twist.linear.x = 0.7
+    pub = rospy.Publisher('/cmd_vel',Twist , queue_size=1)
+    r = rospy.Rate(2)
+    while not rospy.is_shutdown(): 
+        pub.publish(twist)
+        r.sleep()
 
 
 def move_square():
     print('COMPLETE')
 
 if __name__ == "__main__":
-    move_line()
+    move_circle()
